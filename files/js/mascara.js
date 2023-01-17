fb5aa460713906bb6120015f7eb266ccab099ea1
julio-caba <correonuevoparalaoficina@gmail.com>
2023-01-17 11:01:27 -0300

    formulario google

--- files/js/mascara.js
+++ files/js/mascara.js
@@ -1,4 +1,19 @@
-$("input[name='masknumber']").on("input", function(){
+$("input[name='entry.411213780']").on("input", function(){
+    let n = destroyMask(this.value);
+      this.setAttribute("data-normalized", n);
+    this.value = createMask(n);
+  })
+  
+  function createMask(string){
+      return string.replace(/(\d{2})(\d{3})(\d{2})/,"$1$2$3");
+  }
+  
+  function destroyMask(string){
+      return string.replace(/\D/g,'').substring(0, 10);
+  }
+
+
+  $("input[name='entry.1339548199']").on("input", function(){
     let n = destroyMask(this.value);
       this.setAttribute("data-normalized", n);
     this.value = createMask(n);
